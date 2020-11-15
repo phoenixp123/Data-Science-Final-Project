0b2d4d11dfc9c2a8ee68fbdb4ba7189c52915d3a
phoenixp123 <ppagan@clarku.edu>
2020-11-14 19:43:21 -0500

    Added functionality for getting location data from users.

--- tweet_scaper.py
+++ tweet_scaper.py
@@ -1,14 +1,12 @@
 import tweepy
-import json
-import csv
+import math
+
 import pandas as pd
 import numpy as np
 from google.cloud import language
 from google.cloud.language import types
 from google.cloud.language import enums
-import requests
-from bs4 import BeautifulSoup
-from collections import Counter,defaultdict
+# from collections import Counter,defaultdict
 import matplotlib.pyplot as plt
 
 consumer_key = "OROBANkVOsvva9HETWL4Kovbx"
@@ -24,12 +22,29 @@ auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit = True)
 
-class TweetScraper():
-    def __init__(self, ):
+
+
+# Data Collection &  Processing
+# ---------------------------------------------------------------------------------------------------------------------
+
+def get_coordinates(filename):
+    lat_long = pd.read_csv(filename,usecols = ["latitude","longitude","country"])
+    lat_long["country"] = lat_long["country"].astype(str).apply(lambda s: s.strip())
+    lat_long = lat_long[["country","latitude","longitude"]]
+    lat_long = lat_long.sort_values(by = "country")
+
+    radius = pd.read_csv("countries of the world.csv",usecols = ["Country","Area (sq. mi.)"])
+    radius["Country"] = radius["Country"].astype(str).apply(lambda s: s.strip())
+    radius["Area (sq. mi.)"] = radius["Area (sq. mi.)"].astype(float).apply(lambda x: math.sqrt(x))
+    radius.columns = ["country","area"]
         
 
+    coordinates = pd.merge(lat_long,radius,on = ["country"])
 
-def read_csv(filename):
+    return coordinates
+
+
+def read_csv_hfi(filename):
     df = pd.read_csv(filename,usecols = ["countries","region","hf_score","hf_rank","hf_quartile",
                                          "pf_expression","pf_association_assembly","pf_movement"],na_values = ["-"])
 
@@ -40,15 +55,24 @@ def read_csv(filename):
 
     df_aggregate = df.groupby(by = ["countries"]).aggregate(aggregation_functions)
 
-    return df, df_aggregate
+    return df,df_aggregate
 
 
-def eda_visualizations(dataframe):
+# Twitter Interaction and Feature Extraction
+# ---------------------------------------------------------------------------------------------------------------------
+
+# get_tweet(max = 100, location = api.geosearch(df["lat].....df["area"])
+# return tweets
+
+
+# Visualizations
+# ---------------------------------------------------------------------------------------------------------------------
+
+def visualizations(dataframe):
     dataframe = dataframe.sort_values(by = "hf_score",ascending = False)
 
     plt.figure(figsize = (20,6))
-    plt.bar(x = dataframe["region"],height = dataframe["hf_score"], label= "Average HFI Score")
-    # plt.plot(dataframe["region"],dataframe["hf_rank"], label = "HFI Rank")
+    plt.bar(x = dataframe["region"],height = dataframe["hf_score"],label = "Average HFI Score")
 
     plt.ylabel("Average HFI Score")
     plt.xlabel("Region")
@@ -56,8 +80,15 @@ def eda_visualizations(dataframe):
     plt.legend()
     plt.show()
 
+# Machine Learning and Sentiment Analysis
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Final Report and Presentation
+# ---------------------------------------------------------------------------------------------------------------------
+
 
 if __name__ == "__main__":
-    df, df_aggregate = read_csv("hfi_cc_2019.csv")
-    print(df_aggregate)
-    eda_visualizations(df_aggregate)
+    df,df_aggregates = read_csv_hfi("hfi_cc_2019.csv")
+    df_coordinates = get_coordinates("lat_long_coordinates.csv")
+    # visualizations(df_aggregates)
+    print(df_coordinates)
